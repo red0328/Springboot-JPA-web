b8a77bec79cb186301456d711ad06e4ec5771fe3
red0328 <ksundoung@naver.com>
2021-04-14 21:00:08 +0900

    환경세팅 완료

---
+++ src/test/java/com/cos/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cos.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
